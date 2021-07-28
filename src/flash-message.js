3e4a4a25a6b7b8e73fe93b6aafcd66d89ff58126
PedroHenrique-git <pedromf34@outlook.com>
2021-07-28 00:05:52 -0300

    events

--- src/flash-message.js
+++ src/flash-message.js
@@ -1,6 +1,12 @@
+/*!
+ * FlashMessage
+ * @license MIT licensed
+ *
+ * Copyright (C) 2021 Pedro Henrique
+ */
+
 /* eslint-disable no-param-reassign */
 /* eslint-disable func-names */
-
 (function (root, factory) {
   root.FlashMessage = factory();
 }(this, () => {
@@ -8,6 +14,7 @@
   const FlashMessage = function (options) {
     this.options = options || {};
     this.container = document.createElement('section');
+    this.duration = this.options.duration || 3000;
     this.container.classList.add('flash-container');
 
     if (positions.includes(this.options.position)) {
@@ -19,23 +26,49 @@
     document.body.appendChild(this.container);
   };
 
+  function Timer(callback, delay) {
+    this.start = 0;
+    this.timer = 0;
+    this.remaining = delay;
+
+    this.resume = function () {
+      this.start = new Date();
+      this.timer = window.setTimeout(callback, this.remaining);
+    };
+
+    this.pause = function () {
+      this.remaining -= new Date() - this.start;
+      window.clearTimeout(this.timer);
+    };
+
+    this.resume();
+  }
+
   FlashMessage.prototype = {
     constructor: FlashMessage,
 
-    hideFlash(flash) {
-      setTimeout(() => {
+    controlFadeAndHover(flash) {
+      const timer = new Timer(() => {
         flash.classList.add('flash-hidden');
-      }, this.options.duration || 3000);
+      }, this.duration);
 
-      flash.addEventListener('transitionend', function () {
-        this.remove();
+      flash.addEventListener('mouseover', () => {
+        timer.pause();
+      });
+
+      flash.addEventListener('mouseout', () => {
+        timer.resume();
+      });
+
+      flash.addEventListener('transitionend', () => {
+        flash.remove();
       });
     },
 
     createFlash(type) {
       const flash = document.createElement('div');
       flash.className = `flash ${type}`;
-      this.hideFlash(flash);
+      this.controlFadeAndHover(flash);
       return flash;
     },
 
